b495bd2865150567e3d8649f7073625d71b24a85
LinaresGD <209667663+LinaresGD@users.noreply.github.com>
2026-02-16 16:46:40 +0100

    [ILI]FastEnemy

---
+++ Assets/Scripts/FastEnemy.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class FastEnemy : MonoBehaviour
+{
+    [Header("Grid Movement Settings")]
+    [SerializeField] private float gridSize = 0.5f;
+    [SerializeField] private float stepDuration = 0.15f;
+    [SerializeField] private float destroyY = -6f;
+    [SerializeField] private float groundY = -4.5f;
+
+    [Header("Score Value")]
+    [SerializeField] private int scoreValue = 10;
+
+    private float columnX;
+    private bool columnSet = false;
+    private bool wasCollected = false;
+    private float nextStepTime;
+    private float currentGridY;
+
+    public void SetColumn(float xPosition)
+    {
+        columnX = xPosition;
+        columnSet = true;
+        currentGridY = Mathf.Round(transform.position.y / gridSize) * gridSize;
+        transform.position = new Vector3(columnX, currentGridY, transform.position.z);
+        nextStepTime = Time.time + stepDuration;
+    }
+
+    public void SetStepDuration(float duration)
+    {
+        stepDuration = duration * 0.5f;
+    }
+
+    private void Update()
+    {
+        if (!columnSet || wasCollected) return;
+
+        if (Time.time >= nextStepTime)
+        {
+            currentGridY -= gridSize;
+            transform.position = new Vector3(columnX, currentGridY, transform.position.z);
+            nextStepTime = Time.time + stepDuration;
+        }
+
+        if (!wasCollected && currentGridY <= groundY)
+        {
+            MissedEnemy();
+        }
+
+        if (currentGridY < destroyY)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !wasCollected)
+        {
+            Collect();
+        }
+    }
+
+    private void Collect()
+    {
+        wasCollected = true;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AddScore(scoreValue);
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void MissedEnemy()
+    {
+        wasCollected = true;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LoseLife();
+        }
+
+        Destroy(gameObject);
+    }
+}
